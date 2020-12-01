d9b32189201e096857155b25bdafa9d5bf6a2ed0
Matej Marinko <matejmarinko123@gmail.com>
2020-12-01 15:14:40 +0100

    fix milestone info urls

--- internal/handle/demo.go
+++ internal/handle/demo.go
@@ -9,7 +9,7 @@ func MilestoneInfoHandle(w http.ResponseWriter, _ *http.Request) {
 	res, _ := json.Marshal(map[string]interface{}{
 		"clani":         []string{"mm8155", "vd3217"},
 		"opis_projekta": "Sistem za vodenje trenutnih projektov, izplačil in inventarja podjetja.",
-		"mikrostoritve": []string{"http://10.244.0.20/api/v1/items", "http://10.0.195.94:8080/api/v1/users"},
+		"mikrostoritve": []string{"http://20.62.232.75/api/v1/items", "http://20.62.234.124:8080/api/v1/users"},
 		"github":        []string{"https://github.com/zerodays/sistem-inventory", "https://github.com/zerodays/sistem-users"},
 		"travis":        []string{"https://travis-ci.org/github/zerodays/sistem-inventory", "https://github.com/zerodays/sistem-users"},
 		"dockerhub":     []string{"https://hub.docker.com/r/zdays/sistem-inventory", "https://hub.docker.com/r/zdays/sistem-users"},
